f4c309d7993b8b82c62c514d02de136b3ca248dc
Kareem Assad <kareemassad5@gmail.com>
2020-02-11 23:39:35 -0500

    commiting here before creating the arraylist and linkedlist methods.

--- Assignment_#2/countStringInText.java
+++ Assignment_#2/countStringInText.java
@@ -1,9 +1,17 @@
 /*
-@Author: Kareem El Assad
-@Date: 2/11/2020
+    @Description: This class counts the number of times a certain string appears in a large text file. It also compares the elapsed times to completion of a LinkedList approach to an ArrayList approach.
+    @Author: Kareem El Assad
+    @Date: 2/11/2020
 */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.io.*;
+import java.util.Scanner;
+
 
 public class countStringInText {
     
@@ -27,7 +35,28 @@ public class countStringInText {
         return -1; // search failed
     }
     /* 
+    A method that reads a file and checks it line by line
     */
+    public static int searchListSpecificWord(String filename, Object K) throws IOException {
+        if(!(K instanceof LinkedList) && !(K instanceof ArrayList)){
+            throw new IllegalArgumentException("Invalid type provded");
+        }
+        int count;
+        String[] wordsFromLine;
+        BufferedReader read = new BufferedReader(new FileReader(filename));
+
+        try {
+            String line = read.readLine();
+
+            while (line != null) {
+                //TODO: Create the methods for creating a linkedlist and an Array list BEFORE proceeding.
+                
+            }
+            
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
 
     public static void main(String[] args) {
 
