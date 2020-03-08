0bd0aa34a65ac5b23ee2f3db5c48c1469fb90f45
Kareem Assad <kareemassad5@gmail.com>
2020-03-08 15:54:58 -0400

    got part one working

--- Assignment_#3/assignment3/LanguageRecognizerG.java
+++ Assignment_#3/assignment3/LanguageRecognizerG.java
@@ -1,43 +1,72 @@
 package assignment3;
 
-import java.awt.*;
-import java.lang.reflect.Array;
+// import java.awt.*;
+// import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.ListIterator;
 
-import org.graalvm.compiler.word.Word;
-
 public class LanguageRecognizerG {
     
-    String InputWord = "";
+    String userInput = "";
     ArrayList<Character> CharacterList = new ArrayList<Character>();
     int initialSize;
 
     LanguageRecognizerG(String input){
-        InputWord = input;
+        userInput = input;
         for (char c : input.toCharArray()) {
             CharacterList.add(c);
         }
         initialSize = CharacterList.size();
-        ListIterator WordIterating = CharacterList.listIterator(0);
+        // ListIterator WordIterating = CharacterList.listIterator(0);
     }
 
-    private boolean recursiveRecogR(ArrayList<Character> WordList) {
+    private boolean recursiveRecogR(ArrayList<Character> condList) {
         //empty string
-        if (WordList.size() == 0) {
+        if (condList.size() == 0) {
             return true;
         }
         // & or # case
-        if (WordList.size() == 1) {
-            if (WordList.get(0) == '&' | WordList.get(0) == '#') {
+        if (condList.size() == 1) {
+            if (condList.get(0) == '&' | condList.get(0) == '#') {
                 return true;
             }
         }
         //W|A and &|#
-        if (WordList.size() == 2) {
-            if ((WordList.get(0) == 'W' | WordList.get(0) == 'A') && (WordList.get(1) == '&' | WordList.get(1) == '#')) {
-                return true;
+        if (condList.size() == 2) {
+            if (condList.get(0) == 'W' | condList.get(0) == 'A') {
+                if (condList.get(1) == '&' | condList.get(1) == '#') {
+                    return true;
+                }
+            }
+            if (condList.get(0) == '&' | condList.get(0) == '#') {
+                if (condList.get(1) == 'W' | condList.get(1) == 'A') {
+                    return true;
+                }
             }
         }
+        // &|# <G> W|A
+        if (condList.size() > 2) {
+            if (condList.get(0) == '&' | condList.get(0) == '#') {
+                if (condList.get(condList.size() - 1) == 'W' | condList.get(condList.size() - 1) == 'A') {
+                    ArrayList fixList = (ArrayList) condList.subList(1,condList.size() -2);
+                    if (recursiveRecogR(fixList)) {
+                        return true;
+                    }
+                    
+                }
+                
+            }
+        }
+        return false;
     }
+    //prof said to use this
+    public void recursivePrintG(){
+        boolean checker = recursiveRecogR(CharacterList);
+        if (checker) {
+            System.out.println("Recusion: Word \"" + userInput + "\" Is a word of the G language.");           
+        }else{
+            System.out.println("Recusion: Word \"" + userInput + "\" Is NOT a word of the G language.");
+        }
+    }
+    
 }
