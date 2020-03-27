66d311546efd1ca9298b1d32b384b8c904c03ea6
Kareem Assad <kareemassad5@gmail.com>
2020-03-27 00:30:32 -0400

    finished assignment4 still needs commenting and prettifying

--- Assignment_#4/Assignment4.java
+++ Assignment_#4/Assignment4.java
@@ -60,5 +60,60 @@ class Assignment4 {
             }
         }
     }
+    //wasnt this on the midterm?
+    /**
+     * This program stores the part of the word before the $ in a queue and the part after the
+     * $ in the stack. It will then check if the queue and stack have the same size. If they do,
+     * it will then remove a letter from the queue and the stack. If they are the same all the way
+     * then the string is in the language otherwise it is not.
+     * 
+     * @param str string given
+     * @return true if the string is in the language
+     */
+    public static boolean isInLanguage (String str) {
+        //need to use both a queue and a stack
+        //dont need to put second types are java (should) infer type
+        Queue<Character> QQ = new LinkedList<>();
+        Stack<Character> SS = new Stack<>();
 
+        int lengthString = str.length();
+        int i = 0;
+
+        //Base case. All chars will be in order until $. They will only need to be reversed after the $.
+        while(str.charAt(i) != '$') {
+            //add current char to QQ
+            QQ.add(str.charAt(i)); 
+            i++;
+        }
+        // This is very important as to get past the $ condition when the loop above stops
+        i++;
+        //fill the stack with the rest
+        while (i < lengthString) {
+            SS.add(str.charAt(i));
+            i++;
+        }
+        //check first case if they are equal size, if not we know it is not in the language
+        if (QQ.size() == SS.size()) {
+            for(i = 0; i < QQ.size(); i++) {
+                // Head of queue removed should be equal top of the stack. Fail otherwise.
+                if (QQ.remove() != SS.pop()) {
+                    return false;
+                }
+            }
+            //if all above passed.
+            return true;
+        }
+
+        return true;
+    }
+
+    public static int convertToNumber (String str) {
+        int n = 0;
+        for (int index = 0; index < str.length(); index++) {
+            if (str.charAt(index) != ' ') {
+                n = (n*10) + Character.getNumericValue(str.charAt(index));
+            }
+        }
+        return n;
+    }
 }
